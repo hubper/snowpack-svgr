19deb5db2553e0377ebe2f0593f52cc16ecc6076
Koen Poelhekke <info@dotred.nl>
2021-05-06 17:53:10 +0200

    Add snowpack plugin

---
+++ test/index.test.ts
@@ -0,0 +1,38 @@
+import {join} from 'path';
+import snowpackSvgr from './index';
+
+describe('snowpack-svgr', () => {
+    it('should compile .svg files', async () => {
+        const filePath = join(__dirname, '__fixtures__/test.svg');
+
+        const plugin = snowpackSvgr({});
+        const result = await plugin.load({filePath});
+
+        expect(result).toMatchSnapshot();
+    });
+
+    it('should be able to use the rollup url plugin', async () => {
+        const filePath = join(__dirname, '__fixtures__/test.svg');
+
+        const plugin = snowpackSvgr({}, {rollupUrlPlugin: true});
+        const result = await plugin.load({filePath});
+
+        expect(result).toMatchSnapshot();
+    });
+
+    it('should be able to handle large svg files', async () => {
+        const filePath = join(__dirname, '__fixtures__/large.svg');
+
+        const plugin = snowpackSvgr(
+            {
+                buildOptions: {
+                    out: 'build',
+                },
+            },
+            {rollupUrlPlugin: true}
+        );
+        const result = await plugin.load({filePath});
+
+        expect(result).toMatchSnapshot();
+    });
+});
