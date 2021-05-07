4f91cdf90b9c5c4bb37c7127f86892bdd572ac21
Koen Poelhekke <info@dotred.nl>
2021-05-07 10:55:21 +0200

    Create monorepo with example

---
+++ packages/example/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo, { ReactComponent as Logo } from './logo.svg';
+import large from './large.svg';
+
+function App() {
+  // Return the App component.
+  return (
+    <>
+      <h2>ReactComponent</h2>
+      <Logo />
+
+      <h2>Inline base64</h2>
+      <img src={logo} alt="" />
+
+      <h2>Inline url</h2>
+      <img src={large} alt="" />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/snowpack-svgr/src/index.ts
@@ -0,0 +1,88 @@
+import { promises as fs } from 'fs';
+import { Plugin, rollup, RollupOptions } from 'rollup';
+// @ts-expect-error
+import svgr from '@svgr/rollup';
+// @ts-expect-error
+import url from '@rollup/plugin-url';
+import { PluginLoadOptions, SnowpackConfig } from 'snowpack';
+import { posix } from 'path';
+
+type SnowpackSVGROptions = {
+  rollupUrlPlugin?: boolean;
+  svgrOptions?: Record<string, unknown>;
+  rollupOptions?: RollupOptions;
+};
+
+function snowpackSvgr(
+  snowpackConfig: SnowpackConfig,
+  {
+    rollupUrlPlugin = false,
+    svgrOptions = {},
+    rollupOptions = {},
+  }: SnowpackSVGROptions = {}
+) {
+  return {
+    name: 'snowpack-svgr',
+    resolve: {
+      input: ['.svg'],
+      output: ['.js', '.svg'],
+    },
+    async load({ filePath, fileExt }: PluginLoadOptions) {
+      let plugins: Plugin[] = [
+        svgr({
+          svgo: true,
+          ref: true,
+          ...svgrOptions,
+        }),
+      ];
+
+      if (rollupUrlPlugin) {
+        let publicPath = posix.sep;
+
+        for (const [start, { url }] of Object.entries(snowpackConfig.mount)) {
+          if (filePath.startsWith(start)) {
+            publicPath = url + publicPath;
+          }
+        }
+
+        plugins.unshift(
+          url({
+            emitFiles: false,
+            destDir: snowpackConfig.buildOptions.out,
+            publicPath,
+            fileName: '[name][extname][extname]',
+          })
+        );
+      }
+
+      const bundle = await rollup({
+        input: filePath,
+        external: ['react'],
+        plugins: [...plugins, ...(rollupOptions.plugins ?? [])],
+        ...rollupOptions,
+      });
+
+      const { output } = await bundle.generate({
+        format: 'es',
+      });
+
+      const result = output[0].code.replace(
+        'import * as React',
+        'import React'
+      );
+
+      if (rollupUrlPlugin) {
+        return {
+          '.js': result,
+          [fileExt]: await fs.readFile(filePath),
+        };
+      }
+
+      return {
+        '[.js]': result,
+      };
+    },
+  };
+}
+
+export default snowpackSvgr;
